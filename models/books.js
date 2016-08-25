999e263e24af50dadecf29a0e1862fd25a060076
leeungmo <ungmo2@gmail.com>
2016-08-26 07:45:14 +0900

    updated

--- models/books.js
+++ models/books.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 
 var booksSchema = new Schema({
-  title          : String,
-  author         : String,
-  published_date : { type: Date, default: Date.now  }
+  title : String,
+  author: String,
+  price : Number
 });
 
 module.exports = mongoose.model('books', bookSchema);
